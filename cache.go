77e6e50f49c322b6c89118e267a38053a02fdf96
Khalid <dzhamaldaev@internet.ru>
2023-01-24 22:54:41 +0300

    Load() реализован и Store() обновлён

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"sync"
@@ -32,7 +33,7 @@ func (c *cache) Get(key string) (interface{}, error) {
 	if val, ok := c.data.Load(key); ok {
 		return val, nil
 	} else {
-		return nil, fmt.Errorf("Get error: there is no such key '%s' in storage", key)
+		return nil, fmt.Errorf("Get() error: there is no such key '%s' in storage", key)
 	}
 }
 
@@ -41,26 +42,45 @@ func (c *cache) Delete(key string) error {
 		c.data.Delete(key)
 		return nil
 	} else {
-		return fmt.Errorf("Delete error: there is no such key '%s' in storage", key)
+		return fmt.Errorf("Delete() error: there is no such key '%s' in storage", key)
 	}
 }
 
 func (c *cache) Store() error {
 	file, err := os.OpenFile("./bag.txt", os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
-		return err
+		return fmt.Errorf("Store() error: %v", err)
 	}
 
 	defer file.Close()
 
+	m := make(map[string]interface{})
+
 	c.data.Range(func(key, value any) bool {
-		_, err = file.Write([]byte(fmt.Sprintf("%v: %v\n", key, value)))
-		return err == nil
+		m[key.(string)] = value
+		return true
 	})
-	
+
+	err = json.NewEncoder(file).Encode(m)
 	if err != nil {
-		return err
+		return fmt.Errorf("Store() error: %v", err)
 	}
 
 	return nil
+}
+
+func (c *cache) Load(ttl time.Duration) error {
+	file, err := os.OpenFile("./bag.txt", os.O_RDWR, 0755)
+	if err != nil {
+		return fmt.Errorf("Load() error: %v", err)
+	}
+
+	m := make(map[string]interface{})
+	json.NewDecoder(file).Decode(&m)
+
+	for key, value := range m {
+		c.Set(key, value, ttl)
+	}
+
+	return err
 }
